cac1406626ae573011c5aa952ab1f7d06c5acb47
J.T. Robinson <jaittarius@hotmail.com>
2019-07-27 20:10:47 -0400

    Updated listings images

--- assets/js/realEstate/data/listingsData.js
+++ assets/js/realEstate/data/listingsData.js
@@ -11,7 +11,7 @@ var listingsData = [
             'gym'
         ],
         hometype: 'Condominium',
-        image: 'https://assets.dnainfo.com/photo/2017/5/1495581226-299792/larger.jpg'
+        image: '/img/larger.jpg'
     },
     {
         address: '2348  Nixon Avenue',
@@ -26,7 +26,7 @@ var listingsData = [
             'gym'
         ],
         hometype: 'Condominium',
-        image: 'https://www.amli.com/AMLIcontent/files/apartments/dallas/escena/amenity-exterior/escena-amenity-exterior-building-exterior-2_B.jpg?w=320&h=237'
+        image: '/img/escena-amenity-exterior-building-exterior-2_B.jpg?w=320&h=237'
     },
     {
         address: '768  Wilkinson Street',
@@ -40,7 +40,7 @@ var listingsData = [
             'finished basement'
         ],
         hometype: 'Single-family home',
-        image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/brewster-mcleod-architects-1486154143.jpg?crop=1xw:1xh;center,top&resize=480:*'
+        image: '/img/brewster-mcleod-architects-1486154143.jpg?crop=1xw:1xh;center,top&resize=480:*'
     },
     {
         address: '3859  Walt Nuzum Farm Road',
@@ -54,7 +54,7 @@ var listingsData = [
             'gym'
         ],
         hometype: 'Single-family home',
-        image: 'https://ap.rdcpix.com/462642208/ddcf38d12e7d148a3cc0f20a189b7f68l-m0xd-w480_h480_q80.jpg'
+        image: '/img/ddcf38d12e7d148a3cc0f20a189b7f68l-m0xd-w480_h480_q80.jpg'
     },
     {
         address: '4625  Kincheloe Road',
@@ -69,7 +69,7 @@ var listingsData = [
             'gym'
         ],
         hometype: 'Townhome',
-        image: 'https://i.pinimg.com/736x/da/95/01/da9501057f20f97a2594f61d382bbd51--luxury-townhomes-brick-facade.jpg'
+        image: '/img/da9501057f20f97a2594f61d382bbd51--luxury-townhomes-brick-facade.jpg'
     },
     {
         address: '3302  Christie Way',
@@ -82,7 +82,7 @@ var listingsData = [
             'finished basement'
         ],
         hometype: 'Townhome',
-        image: 'https://ap.rdcpix.com/1300778947/707abb9bbff253667c53d5de726f4b06l-m0xd-w480_h480_q80.jpg'
+        image: '/img/707abb9bbff253667c53d5de726f4b06l-m0xd-w480_h480_q80.jpg'
     }
 
 ]
--- public/js/components/realEstate.js
+++ public/js/components/realEstate.js
@@ -18,7 +18,7 @@ var listingsData = [{
     floorspace: 2200,
     extras: ['elevator', 'gym'],
     hometype: 'Condominium',
-    image: 'https://assets.dnainfo.com/photo/2017/5/1495581226-299792/larger.jpg'
+    image: '/img/larger.jpg'
 }, {
     address: '2348  Nixon Avenue',
     city: 'Charlotte',
@@ -28,7 +28,7 @@ var listingsData = [{
     floorspace: 1800,
     extras: ['elevator', 'swimming pool', 'gym'],
     hometype: 'Condominium',
-    image: 'https://www.amli.com/AMLIcontent/files/apartments/dallas/escena/amenity-exterior/escena-amenity-exterior-building-exterior-2_B.jpg?w=320&h=237'
+    image: '/img/escena-amenity-exterior-building-exterior-2_B.jpg?w=320&h=237'
 }, {
     address: '768  Wilkinson Street',
     city: 'Huntersville',
@@ -38,7 +38,7 @@ var listingsData = [{
     floorspace: 3200,
     extras: ['swimming pool', 'finished basement'],
     hometype: 'Single-family home',
-    image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/brewster-mcleod-architects-1486154143.jpg?crop=1xw:1xh;center,top&resize=480:*'
+    image: '/img/brewster-mcleod-architects-1486154143.jpg?crop=1xw:1xh;center,top&resize=480:*'
 }, {
     address: '3859  Walt Nuzum Farm Road',
     city: 'Knoxville',
@@ -48,7 +48,7 @@ var listingsData = [{
     floorspace: 1800,
     extras: ['finished basement', 'gym'],
     hometype: 'Single-family home',
-    image: 'https://ap.rdcpix.com/462642208/ddcf38d12e7d148a3cc0f20a189b7f68l-m0xd-w480_h480_q80.jpg'
+    image: '/img/ddcf38d12e7d148a3cc0f20a189b7f68l-m0xd-w480_h480_q80.jpg'
 }, {
     address: '4625  Kincheloe Road',
     city: 'Hampton',
@@ -58,7 +58,7 @@ var listingsData = [{
     floorspace: 1800,
     extras: ['elevator', 'swimming pool', 'gym'],
     hometype: 'Townhome',
-    image: 'https://i.pinimg.com/736x/da/95/01/da9501057f20f97a2594f61d382bbd51--luxury-townhomes-brick-facade.jpg'
+    image: '/img/da9501057f20f97a2594f61d382bbd51--luxury-townhomes-brick-facade.jpg'
 }, {
     address: '3302  Christie Way',
     city: 'Rock Hill',
@@ -68,7 +68,7 @@ var listingsData = [{
     floorspace: 1600,
     extras: ['finished basement'],
     hometype: 'Townhome',
-    image: 'https://ap.rdcpix.com/1300778947/707abb9bbff253667c53d5de726f4b06l-m0xd-w480_h480_q80.jpg'
+    image: '/img/707abb9bbff253667c53d5de726f4b06l-m0xd-w480_h480_q80.jpg'
 }];
 
 exports.default = listingsData;
